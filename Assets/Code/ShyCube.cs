a78384417ad16b42413b61319b8e805119eca81b
Jordan <jyodann@outlook.com>
2026-03-16 00:29:06 +0800

    Added ShyCube quest

---
+++ Assets/Code/ShyCube.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShyCube : MonoBehaviour
+{
+    [SerializeField]
+    private string MySecret = "i_like_you";
+}
